ce27c7eace3751fe8aeae994d6089075ce3e757c
Cheng7871 <2042264067@qq.com>
2026-09-13 21:14:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
-// 托管静态html文件，index.html、zjh.html都能直接访问
 app.use(express.static(path.join(__dirname, './')));
 
 const server = http.createServer(app);
@@ -14,16 +13,18 @@ console.log("WebSocket 服务启动成功！");
 
 wss.on('connection', (ws) => {
   console.log("有玩家连上");
-  ws.on('message', (raw) => {
-    // 收到消息，广播给所有在线人
+  ws.on('message', (rawData) => {
+    // 强制转成字符串文本，解决Blob二进制乱码问题
+    let msg = rawData.toString();
+    // 广播文本给所有人
     wss.clients.forEach(client => {
       if(client.readyState === WebSocket.OPEN){
-        client.send(raw);
+        client.send(msg);
       }
     })
   })
   ws.on('close', ()=>{
-    console.log("玩家断开")
+    console.log("玩家断开连接")
   })
 })
 
