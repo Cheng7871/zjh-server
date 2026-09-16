80befca9d8e9632eb2e35074fbfd4b0ce9afe7d2
Cheng7871 <2042264067@qq.com>
2026-09-16 09:26:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -373,6 +373,7 @@ wss.on('connection', (ws) => {
     })
 })
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("服务器启动，端口：" + PORT);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log("服务器启动, 端口: " + PORT);
 });
+
